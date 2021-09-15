d63ba6a4216fe381ad226c7b5a6f7125e97e1f45
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-09-15 03:12:55 +0200

    ya

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { useEffect, useRef } from "react";
 import {
   AmbientLight,
+  BoxGeometry,
+  CubeTextureLoader,
   DirectionalLight,
+  DoubleSide,
+  Mesh,
+  MeshStandardMaterial,
   PCFShadowMap,
   PerspectiveCamera,
+  PlaneGeometry,
   Scene,
   WebGLRenderer,
 } from "three";
@@ -18,11 +24,12 @@ function App() {
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = PCFShadowMap;
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const aspect = window.innerWidth / window.innerHeight;
+    renderer.setSize(window.innerWidth * 0.95, window.innerHeight * 0.95);
 
     //camera
     const fov = 60;
-    const aspect = 1920 / 1000;
     const near = 1.0;
     const far = 1000.0;
     const camera = new PerspectiveCamera(fov, aspect, near, far);
@@ -52,27 +59,101 @@ function App() {
 
     scene.add(light);
 
+    //skybox
+    // 'px.png'
+    // 'nx.png'
+    // 'py.png'
+    // 'ny.png'
+    // 'pz.png'
+    // 'nz.png'
+    const loader = new CubeTextureLoader();
+    const texture = loader.load([
+      "SpaceboxCollection/Spacebox1/_left.png",
+      "SpaceboxCollection/Spacebox1/_right.png",
+
+      "SpaceboxCollection/Spacebox1/_top.png",
+      "SpaceboxCollection/Spacebox1/_bottom.png",
+
+      "SpaceboxCollection/Spacebox1/_front.png",
+      "SpaceboxCollection/Spacebox1/_back.png",
+    ]);
+    scene.background = texture;
+
+    //add a plane
+    const plane = new Mesh(
+      new PlaneGeometry(100, 100, 2, 2),
+      new MeshStandardMaterial({
+        color: "white",
+        transparent: false,
+      })
+    );
+
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    plane.material.side=DoubleSide;
+    scene.add(plane);
+
+    //add  a box
+    const box = new Mesh(
+      new BoxGeometry(2, 2, 2),
+      new MeshStandardMaterial({
+        color: "gray",
+      })
+    );
+    box.position.set(0, 2, 0);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    scene.add(box);
+
+    ////////
+
+    //add boxes
+    for (let x = -8; x < 8; x++) {
+      for (let y = -8; y < 8; y++) {
+        const box = new Mesh(
+          new BoxGeometry(2, 2, 2),
+          new MeshStandardMaterial({
+            color: 0x808080,
+          })
+        );
+        box.position.set(
+          Math.random() + x * 5,
+          Math.random() * 14.0 + 2.0,
+          Math.random() + y * 5
+        );
+        box.castShadow = true;
+        box.receiveShadow = true;
+        scene.add(box);
+      }
+    }
+
     const onResize = () => {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
     };
 
+    //animate
     let frameId;
     const RAF = () => {
-      frameId = requestAnimationFrame(() => {
-        renderer.render(scene, camera);
-        RAF();
-      });
+      renderer.render(scene, camera);
+      frameId = requestAnimationFrame(RAF);
     };
 
     //resize
     window.addEventListener("resize", onResize);
 
-    //add renderer to DOM
     const { domElement } = renderer;
-    rendererRef.current.appendChild(domElement);
 
+    //add controls
+
+    const controls = new OrbitControls(camera, domElement);
+    controls.target.set(0, 20, 0);
+    controls.update();
+
+    //add renderer to DOM
+    rendererRef.current.appendChild(domElement);
     //animate
     RAF();
 
@@ -80,11 +161,21 @@ function App() {
     return () => {
       cancelAnimationFrame(frameId);
       window.removeEventListener("resize", onResize);
+      // domElement.removeEventListener("DOMContentLoaded", start);
       domElement.remove();
     };
   }, []);
 
-  return <div ref={rendererRef}></div>;
+  return (
+    <div
+      ref={rendererRef}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    ></div>
+  );
 }
 
 export default App;
